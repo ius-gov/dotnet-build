3f3a9595ff810bf76853921f2ffa9a9ffda2f514
Chris Woerz <erendrake@gmail.com>
2017-05-25 15:04:54 -0600

    i wasnt doing it right

--- build.ps1
+++ build.ps1
@@ -147,9 +147,9 @@ function ExecuteDatabaseBuilds
         else
         {
                 $build.databases| ForEach {
-                  $output = $env:BUILD_ARTIFACTSTAGINGDIRECTORY + "\" + $_.name
+                  $output = "~\" + $_.name
                    Write-Host "MSBuild Database to $output" -ForegroundColor Green
-                   & dotnet msbuild $_.path /p:OutputPath=$output
+                   dotnet msbuild $_.path /p:OutputPath=$output
                   if ($LASTEXITCODE -eq 1)
                   {
                       Write-Host "Error build database $_" -ForegroundColor Red
