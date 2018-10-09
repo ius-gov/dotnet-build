abdd885a30ffe8c07cd19b8a23eb5ebb8ee91c8d
Chris Woerz <chriswoerz@gmail.com>
2018-10-09 11:18:57 -0600

    correct powershell is important

--- build.ps1
+++ build.ps1
@@ -28,9 +28,10 @@ function BumpVersions {
     if (-Not $PSBoundParameters.ContainsKey('build')) {
         $build = Get-Build-Config
     }
+    $buildId = [int]$env:BUILD_BUILDID
 
-    $versionNumber = "$($clientStateFIPS).$($build.version.major).$($build.version.minor).$env:BUILD_BUILDID"
-    if ($env:BUILD_BUILDID -is [int] -And $env:BUILD_BUILDID -gt 65535){
+    $versionNumber = "$($clientStateFIPS).$($build.version.major).$($build.version.minor).$buildId"
+    if ($env:BUILD_BUILDID -gt 65535){
         $buildId = $env:BUILD_BUILDID - 65535
         $versionNumber = "$($clientStateFIPS).$($build.version.major).$($build.version.minor + 1).$buildId"
     }
